c79e575ebd7f4dab5348faf9a25e0973a221dde0
Caíque da Silva Machado <caique9machado@gmail.com>
2022-05-19 17:25:25 -0300

    Aula 01: Exercício 04 - Tipos de dados

---
+++ Exercicio_04/exercicio04.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var sobrenome string = "Silva"
+	var idade int = 25
+	boolean := false
+	var salario string = "4585.90"
+	var nome string = "Fellipe"
+
+	fmt.Println(nome, sobrenome, idade, salario, boolean)
+}
