f73c1a06ed89262e447601853d9a2b0e26ac3ffd
Caíque da Silva Machado <caique9machado@gmail.com>
2022-05-19 17:23:15 -0300

    Aula 01: Exercício 03 - Declaração de variáveis

---
+++ Exercicio_03/exercicio03.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nome string
+	var sobrenome string
+	var idade int
+	sobrenome = "6"
+	var licençaParaDirigir = true
+	var estaturaDaPessoa int
+	quantidadeDeFilhos := 2
+
+	fmt.Println(nome, sobrenome, idade, licençaParaDirigir, estaturaDaPessoa, quantidadeDeFilhos)
+}
