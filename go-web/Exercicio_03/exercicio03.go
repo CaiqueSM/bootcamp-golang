c040b5c6e3f55d13f2c135ce49d4a874298dab03
caiqueSM <caique9machado@gmail.com>
2022-05-29 09:23:52 -0300

    go-web

--- go-web/Exercicio_03/exercicio03.go
+++ go-web/Exercicio_03/exercicio03.go
@@ -2,35 +2,33 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 type usuario struct {
-	id        int     `json: "id"`
-	nome      string  `json: "nome"`
-	sobrenome string  `json: "sobrenome"`
-	email     string  `json: "email"`
-	idade     uint    `json: "idade"`
-	altura    float64 `json: "altura"`
-	ativo     bool    `json: "ativo"`
-	data      string  `json: "data de criação"`
+	Id        int       `json: "id"`
+	Nome      string    `json: "nome"`
+	Sobrenome string    `json: "sobrenome"`
+	Email     string    `json: "email"`
+	Idade     uint      `json: "idade"`
+	Altura    float64   `json: "altura"`
+	Ativo     bool      `json: "ativo"`
+	Data      time.Time `json: "data de criação"`
 }
 
 func GetAll(c *gin.Context) {
-	//u := []usuario{{id: 1, nome: "A", sobrenome: "BCDE", email: "email@generico.com",
-	//	idade: 25, altura: 1.68, ativo: true, data: "28-05-2022"},
-	//	{id: 2, nome: "B", sobrenome: "CDEF", email: "email@generico.com", idade: 25, altura: 1.68,
-	//		ativo: false, data: "28-05-2022"}}
 
-	c.JSON(http.StatusOK, usuario{id: 2, nome: "B", sobrenome: "CDEF", email: "email@generico.com", idade: 25, altura: 1.68,
-		ativo: false, data: "28-05-2022"})
+	var usuarios = []usuario{
+		{Id: 1, Nome: "A", Sobrenome: "BCDE", Email: "email@generico.com", Idade: 25, Altura: 1.68, Ativo: true, Data: time.Now()},
+		{Id: 2, Nome: "B", Sobrenome: "CDEF", Email: "email@generico.com", Idade: 25, Altura: 1.68, Ativo: false, Data: time.Now()}}
+
+	c.IndentedJSON(http.StatusOK, usuarios)
 }
 
 func main() {
-
 	router := gin.Default()
-
 	router.GET("/usuarios", GetAll)
 	router.Run()
 }
