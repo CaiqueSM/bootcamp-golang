cea2f66f5ec4547ca5cd6204a85ca75c9498f154
caiqueSM <caique9machado@gmail.com>
2022-06-01 21:11:44 -0300

    go-web-arquitetura

---
+++ go-web/Aula_04/Exercicio_01/internal/usuarios/repository.go
@@ -0,0 +1,42 @@
+package usuarios
+
+type Usuario struct {
+	Id        int   `json:"id"`
+	Nome      string  `json:"nome" binding:"required"`
+	Sobrenome string  `json:"sobrenome" binding:"required"`
+	Email     string  `json:"email" binding:"required"`
+	Idade     uint    `json:"idade" binding:"required"`
+	Altura    float64 `json:"altura" binding:"required"`
+	Ativo     bool    `json:"ativo" binding:"required"`
+	Data      string  `json:"data" binding:"required"`
+}
+
+var ps []Usuario
+var lastID int
+
+type Repository interface {
+	GetAll() ([]Usuario, error)
+	Store(id int, nome, sobrenome, email string, idade uint, altura float64, ativo bool, data string) (Usuario, error)
+	LastID() (int, error)
+}
+
+type repository struct{}
+
+func NewRepository() Repository {
+	return &repository{}
+}
+
+func (r *repository) GetAll() ([]Usuario, error) {
+	return ps, nil
+}
+
+func (r *repository) Store(id int, nome, sobrenome, email string, idade uint, altura float64, ativo bool, data string) (Usuario, error) {
+	u := Usuario{id, nome, sobrenome, email, idade, altura, ativo, data}
+	ps = append(ps, u)
+	lastID = u.Id
+	return u, nil
+}
+
+func (r *repository) LastID() (int, error) {
+	return lastID, nil
+}
---
+++ go-web/Aula_04/Exercicio_01/internal/usuarios/service.go
@@ -0,0 +1,41 @@
+package usuarios
+
+type Service interface {
+	GetAll() ([]Usuario, error)
+	Store(nome, sobrenome, email string, idade uint, altura float64, ativo bool, data string) (Usuario, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(r Repository) Service {
+	return &service{
+		repository: r,
+	}
+}
+
+func (s *service) GetAll() ([]Usuario, error) {
+	ps, err := s.repository.GetAll()
+	if err != nil {
+		return nil, err
+	}
+	return ps, nil
+}
+
+func (s *service) Store(nome, sobrenome, email string, idade uint, altura float64, ativo bool, data string) (Usuario, error) {
+	lastID, err := s.repository.LastID()
+	if err != nil {
+		return Usuario{}, err
+	}
+
+	lastID++
+
+	usuario, err := s.repository.Store(lastID, nome, sobrenome, email, idade, altura, ativo, data)
+
+	if err != nil {
+		return Usuario{}, err
+	}
+
+	return usuario, nil
+}
