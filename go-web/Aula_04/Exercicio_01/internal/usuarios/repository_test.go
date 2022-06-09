7db82855254e3b8c2b6b4c2b5ce125fcea2ec1f3
caiqueSM <caique9machado@gmail.com>
2022-06-09 08:32:51 -0300

    go-testes aula 03

---
+++ go-web/Aula_04/Exercicio_01/internal/usuarios/repository_test.go
@@ -0,0 +1,104 @@
+package usuarios
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/CaiqueSM/bootcamp-golang.git/go-web/Aula_04/Exercicio_01/pkg/store"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetAll(t *testing.T) {
+	input := []Usuario{
+		{
+			Id:        1,
+			Nome:      "A",
+			Sobrenome: "bcdef",
+			Email:     "generico@email.com",
+			Idade:     50,
+			Altura:    1.89,
+			Ativo:     true,
+			Data:      "07/06/2022",
+		},
+		{
+			Id:        2,
+			Nome:      "B",
+			Sobrenome: "cdefg",
+			Email:     "generico@email.com",
+			Idade:     25,
+			Altura:    1.70,
+			Ativo:     true,
+			Data:      "08/06/2022",
+		},
+	}
+	dataJson, _ := json.Marshal(input)
+	dbMock := store.Mock{
+		Data: dataJson,
+		Err:  nil,
+	}
+	storeStub := store.FileStore{
+		FileName: "",
+		Mock:     &dbMock,
+	}
+
+	myRepo := NewRepository(&storeStub)
+	resp, _ := myRepo.GetAll()
+	assert.Equal(t, input, resp)
+}
+
+func TestUpdateSobrenomeIdade(t *testing.T) {
+	input := []Usuario{
+		{
+			Id:        1,
+			Nome:      "A",
+			Sobrenome: "Before Update",
+			Email:     "generico@email.com",
+			Idade:     50,
+			Altura:    1.89,
+			Ativo:     true,
+			Data:      "07/06/2022",
+		},
+		{
+			Id:        2,
+			Nome:      "B",
+			Sobrenome: "cdefg",
+			Email:     "generico@email.com",
+			Idade:     25,
+			Altura:    1.70,
+			Ativo:     true,
+			Data:      "08/06/2022",
+		},
+	}
+
+	expectedResult := Usuario{
+		Id:        1,
+		Nome:      "A",
+		Sobrenome: "After Update",
+		Email:     "generico@email.com",
+		Idade:     50,
+		Altura:    1.89,
+		Ativo:     true,
+		Data:      "07/06/2022",
+	}
+
+	dataJson, _ := json.Marshal(input)
+	dbMock := store.Mock{
+		Data:          dataJson,
+		Err:           nil,
+		ReadWasCalled: false,
+	}
+	storeStub := store.FileStore{
+		FileName: "/Users/caimachado/Documents/Github/bootcamp-golang/go-web/Aula_04/Exercicio_01/cmd/server/usuarios.json",
+		Mock:     &dbMock,
+	}
+
+	myRepo := NewRepository(&storeStub)
+	resp, err := myRepo.UpdateSobrenomeIdade(1, "After Update", 50)
+	if err!= nil{
+		fmt.Println(err.Error())
+		return
+	}
+	assert.Equal(t, expectedResult, resp)
+	assert.True(t, dbMock.ReadWasCalled)
+}
