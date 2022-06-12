3726060c908166eeb56c5a09a4a322ea513eb4bd
caiqueSM <caique9machado@gmail.com>
2022-06-12 11:24:39 -0300

    Refatorando função fibonacci (TDD)

--- go-teste/Aula_03/fibonacci/fibonacci.go
+++ go-teste/Aula_03/fibonacci/fibonacci.go
@@ -1,11 +1,18 @@
 package fibonacci
 
-func Fibonacci(n int) int64 {
+func Fibonacci(n int)[]int64{
+	terms := make([]int64, n+1)
+	terms[0] = 0
 	if n == 0 {
-		return 0
+		return terms
 	}
-	if n == 1 {
-		return 1
+	terms[1] = 1
+	if n == 1{
+		return terms
 	}
-	return Fibonacci(n-1) + Fibonacci(n-2)
+
+	for i := 2; i <= n; i++{
+		terms[i] = terms[i - 1] + terms[i - 2]
+	}
+	return terms
 }
--- go-teste/Aula_03/fibonacci/fibonacci_test.go
+++ go-teste/Aula_03/fibonacci/fibonacci_test.go
@@ -4,17 +4,18 @@ import (
 	"testing"
 
 	"github.com/CaiqueSM/bootcamp-golang.git/go-teste/Aula_03/fibonacci"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestFibonacci(t *testing.T) {
 	tests := []struct {
 		arg  int
-		want int
-	}{{0, 0},{1, 1}}
+		want []int64
+	}{{0, []int64{0}},{1, []int64{0, 1}},{10, []int64{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55}}}
 
 	for i, d := range tests {
 		got := fibonacci.Fibonacci(d.arg)
-		if got != int64(d.want) {
+		if !assert.Equal(t, got, d.want) {
 			t.Errorf("Test[%d]: fibonacci(%d) returned %d, want %d",
 				i, d.arg, got, d.want)
 		}
